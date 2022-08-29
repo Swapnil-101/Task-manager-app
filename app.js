84162810ac072da399cfe7e32dc88307dd2843b5
Swapnil-101 <Swapniltiwari47@gmail.com>
2022-08-29 10:54:00 +0530

    asdasdfa

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URI);
+    await connectDB('mongodb+srv://sopol:123@cluster0.6ksbyf2.mongodb.net/taskmanager?retryWrites=true&w=majority');
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
